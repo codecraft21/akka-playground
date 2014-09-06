af83997b7ca36fef2ef327b1050efc50005bf97d
Крис <chris@codecraft.ch>
2014-09-06 22:40:31 +0200

    added main helloworld code

--- src/main/scala/ch/codecraft/akkaplayground/AkkaPlayground.scala
+++ src/main/scala/ch/codecraft/akkaplayground/AkkaPlayground.scala
@@ -1,7 +1,6 @@
 package ch.codecraft.akkaplayground
 
-import akka.actor.Actor.Receive
-import akka.actor._
+import akka.actor.{ ActorRef, ActorSystem, Props, Actor, Inbox }
 import scala.concurrent.duration._
 
 /**
@@ -9,16 +8,11 @@ import scala.concurrent.duration._
  *
  * Created by codecraft on 2014-09-06.
  */
-object AkkaPlayground {
-
-  def main(args: Array[String]): Unit = {
-    println("AkkaPlayground")
-  }
-
-}
 
 case object Greet
+
 case class WhoToGreet(who: String)
+
 case class Greeting(message: String)
 
 class Greeter extends Actor {
@@ -35,4 +29,38 @@ class GreetPrinter extends Actor {
   override def receive: Actor.Receive = {
     case Greeting(message) => println(message)
   }
+}
+
+object AkkaPlayground {
+
+  def main(args: Array[String]): Unit = {
+    println("AkkaPlayground starting!")
+    helloWorld()
+    println("AkkaPlayground done!")
+  }
+
+  def helloWorld(): Unit = {
+    val system = ActorSystem("akkaplayground")
+    val greeter = system.actorOf(Props[Greeter], "greeter")
+    val inbox = Inbox.create(system)
+    println("set up")
+
+    greeter.tell(WhoToGreet("codecraft"), ActorRef.noSender)
+    inbox.send(greeter, Greet)
+    println("sent 1")
+
+    val Greeting(message1) = inbox.receive(5.seconds)
+    println(s"Greeting: $message1")
+
+    greeter.tell(WhoToGreet("typesafe"), ActorRef.noSender)
+    inbox.send(greeter, Greet)
+    println("sent 2")
+
+    val Greeting(message2) = inbox.receive(5.seconds)
+    println(s"Greeting: $message2")
+
+    val greetPrinter = system.actorOf(Props[GreetPrinter])
+    system.scheduler.schedule(0.seconds, 1.second, greeter, Greet)(system.dispatcher, greetPrinter)
+  }
+
 }
