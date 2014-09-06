ad1af9fa0f18415f45040652a2707a1a4b5c2856
Крис <chris@codecraft.ch>
2014-09-06 21:44:49 +0200

    added greeter

--- src/main/scala/ch/codecraft/akkaplayground/AkkaPlayground.scala
+++ src/main/scala/ch/codecraft/akkaplayground/AkkaPlayground.scala
@@ -1,12 +1,32 @@
 package ch.codecraft.akkaplayground
 
+import akka.actor.Actor.Receive
+import akka.actor.{ActorRef, ActorSystem, Props, Actor, Inbox}
+import scala.concurrent.duration._
+
 /**
+ * Starting with HelloAkkaScala from activator-hello-akka
+ *
  * Created by codecraft on 2014-09-06.
  */
 object AkkaPlayground {
 
   def main(args: Array[String]): Unit = {
     println("AkkaPlayground")
+  }
+
+}
+
+case object Greet
+case class WhoToGreet(who: String)
+case class Greeting(message: String)
+
+class Greeter extends Actor {
+  var greeting = ""
+
+  override def receive: Receive = {
+    case WhoToGreet(who) => greeting = s"Hello, $who"
+    case Greeting => sender ! Greeting(greeting)
   }
 
 }
