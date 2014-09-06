ac2f734fd21182310cb1e7bcfbdc8556b6d4f0e5
Крис <chris@codecraft.ch>
2014-09-06 20:39:56 +0200

    Initial commit

---
+++ src/main/scala/ch/codecraft/akkaplayground/AkkaPlayground.scala
@@ -0,0 +1,12 @@
+package ch.codecraft.akkaplayground
+
+/**
+ * Created by codecraft on 2014-09-06.
+ */
+object AkkaPlayground {
+
+  def main(args: Array[String]): Unit = {
+    println("AkkaPlayground")
+  }
+
+}
